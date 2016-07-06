16a838148fd4179073d0c93f168e39b24d6479c4
sobolewski <sobolewskide@gmail.com>
2016-07-06 12:23:17 +0200

    ersterTest

--- meinTagScript.ps1
+++ meinTagScript.ps1
@@ -1,4 +1,26 @@
-﻿#########################################
+﻿   $outputExtension = ".mp3"
+   $bitrate = 320
+   $channels = 2
+   
+   foreach($inputFile in get-childitem -recurse -Filter *.wav)
+   { 
+     $outputFileName = [System.IO.Path]::GetFileNameWithoutExtension($inputFile.FullName) + $outputExtension;
+     $outputFileName = [System.IO.Path]::Combine($inputFile.DirectoryName, $outputFileName);
+     
+     $programFiles = ${env:ProgramFiles(x86)};
+     if($programFiles -eq $null) { $programFiles = $env:ProgramFiles; }
+     
+     $processName = $programFiles + "\VideoLAN\VLC\vlc.exe"
+     $processArgs = "-I dummy -vvv `"$($inputFile.FullName)`" --sout=#transcode{acodec=`"mp3`",ab=`"$bitrate`",`"channels=$channels`"}:standard{access=`"file`",mux=`"wav`",dst=`"$outputFileName`"} vlc://quit"
+     
+     start-process $processName $processArgs -wait
+   }
+
+
+
+
+
+#########################################
 # MP3 Tag Update Script
 #
 # Requires Taglib library
